e7ffee59e1e9d884b452c79aba15360a0a470ef8
Jakub Vaněk <vanek.jakub4@seznam.cz>
2019-05-04 20:16:56 +0200

    Exchange params with description in OpCode constructor

--- opcodes.py
+++ opcodes.py
@@ -114,12 +114,14 @@ class VariadicParam:
 
 class OpCode:
     ParamType = Union[PlainParam, LabelParam, VariadicParam]
-    main_code: Tuple[str, int]
-    sub_code: Union[Tuple[str, int], None]
+    Code = Tuple[str, int]
+
+    main_code: Code
+    sub_code: Union[Code, None]
     params: List[ParamType]
     description: str
 
-    def __init__(self, main, sub, params, desc="?"):
+    def __init__(self, main: Code, sub: Union[Code, None], desc: str, params: List[ParamType]):
         self.main_code = main
         self.sub_code = sub
         self.params = params
@@ -172,144 +174,144 @@ OpCodes: List[OpCode] = []
 
 OpCodes += [
     OpCode(("opERROR", 0x00), None,
+           "Terminate the current program and return failure",
            [
 
-           ],
-           "Terminate the current program and return failure"),
+           ]),
 
     OpCode(("opNOP", 0x01), None,
+           "Empty instruction",
            [
 
-           ],
-           "Empty instruction"),
+           ]),
 
     OpCode(("opPROGRAM_STOP", 0x02), None,
+           "Stops specific program id slot",
            [
                T.IN_16("PRGID", "Program id (GUI_SLOT = all, CURRENT_SLOT = current)")
-           ],
-           "Stops specific program id slot"),
+           ]),
 
     OpCode(("opPROGRAM_START", 0x03), None,
+           "Start program id slot",
            [
                T.IN_16("PRGID", "Program id"),
                T.IN_32("SIZE", "Size of image"),
                T.IN_32("*IP", "Address of image (value from opFILE(LOAD_IMAGE,..)  )"),
                T.IN_8("DEBUG", "Debug mode (0=normal, 1=debug, 2=don't execute)")
-           ],
-           "Start program id slot"),
+           ]),
 
     OpCode(("opOBJECT_STOP", 0x04), None,
+           "Stops specific object",
            [
                T.IN_16("OBJID", "Object id")
-           ],
-           "Stops specific object"),
+           ]),
 
     OpCode(("opOBJECT_START", 0x05), None,
+           "Start specific object",
            [
                T.IN_16("OBJID", "Object id")
-           ],
-           "Start specific object"),
+           ]),
 
     OpCode(("opOBJECT_TRIG", 0x06), None,
+           "Triggers object and run the object if fully triggered",
            [
                T.IN_16("OBJID", "Object id")
-           ],
-           "Triggers object and run the object if fully triggered"),
+           ]),
 
     OpCode(("opOBJECT_WAIT", 0x07), None,
+           "Wait until object has run",
            [
                T.IN_16("OBJID", "Object id")
-           ],
-           "Wait until object has run"),
+           ]),
 
     OpCode(("opRETURN", 0x08), None,
+           "Return from byte code subroutine",
            [
 
-           ],
-           "Return from byte code subroutine"),
+           ]),
 
     OpCode(("opCALL", 0x09), None,
+           "Calls byte code subroutine",
            [
                T.IN_16("OBJID", "Object id"),
                T.IN_V_MANY("PARAMETERS", "Subcall arguments")
-           ],
-           "Calls byte code subroutine"),
+           ]),
 
     OpCode(("opOBJECT_END", 0x0A), None,
+           "Stops current object",
            [
 
-           ],
-           "Stops current object"),
+           ]),
 
     OpCode(("opSLEEP", 0x0B), None,
+           "Breaks execution of current VMTHREAD",
            [
 
-           ],
-           "Breaks execution of current VMTHREAD"),
+           ]),
 
     OpCode(("opPROGRAM_INFO", 0x0C), ("OBJ_STOP", 0),
+           "Stop execution of a program object",
            [
                T.IN_16("PRGID", "Program slot number"),
                T.IN_16("OBJID", "Object id")
-           ],
-           "Stop execution of a program object"),
+           ]),
 
     OpCode(("opPROGRAM_INFO", 0x0C), ("OBJ_START", 4),
+           "Start execution of a program object",
            [
                T.IN_16("PRGID", "Program slot number"),
                T.IN_16("OBJID", "Object id")
-           ],
-           "Start execution of a program object"),
+           ]),
 
     OpCode(("opPROGRAM_INFO", 0x0C), ("GET_STATUS", 22),
+           "Read current program status (RUNNING, WAITING, STOPPED, HALTED)",
            [
                T.IN_16("PRGID", "Program slot number"),
                T.OUT_8("DATA", "Program status")
-           ],
-           "Read current program status (RUNNING, WAITING, STOPPED, HALTED)"),
+           ]),
 
     OpCode(("opPROGRAM_INFO", 0x0C), ("GET_SPEED", 23),
+           "Get current program execution speed",
            [
                T.IN_16("PRGID", "Program slot number"),
                T.OUT_32("DATA", "Program speed [instr/S]")
-           ],
-           "Get current program execution speed"),
+           ]),
 
     OpCode(("opPROGRAM_INFO", 0x0C), ("GET_PRGRESULT", 24),
+           "Read program result",
            [
                T.IN_16("PRGID", "Program slot number"),
                T.OUT_8("DATA", "Program result [OK, BUSY, FAIL]")
-           ],
-           "Read program result"),
+           ]),
 
     OpCode(("opPROGRAM_INFO", 0x0C), ("SET_INSTR", 25),
+           "Execute the current thread for the following number of instructions.",
            [
                T.IN_16("COUNT", "Instruction count")
-           ],
-           "Execute the current thread for the following number of instructions."),
+           ]),
 
     OpCode(("opLABEL", 0x0D), None,
+           "Mark this position with a label (32 max per program)",
            [
                T.LABEL("NO", "Label number")
-           ],
-           "Mark this position with a label (32 max per program)"),
+           ]),
 
     OpCode(("opPROBE", 0x0E), None,
+           "Display globals or object locals on terminal",
            [
                T.IN_16("PRGID", "Program slot number"),
                T.IN_16("OBJID", "Object id (zero means globals)"),
                T.IN_32("OFFSET", "Offset (start from)"),
                T.IN_32("SIZE", "Size (length of dump) zero means all (max 1024)")
-           ],
-           "Display globals or object locals on terminal"),
+           ]),
 
     OpCode(("opDO", 0x0F), None,
+           "Run byte code snippet",
            [
                T.IN_16("PRGID", "Program slot number"),
                T.IN_16("*IMAGE", "Address of image"),
                T.IN_16("*GLOBAL", "Address of global variables"),
-           ],
-           "Run byte code snippet"),
+           ]),
 ]
 
 current_code = 0x10
@@ -321,12 +323,12 @@ for op in ["ADD", "SUB", "MUL", "DIV"]:
     for typ, end in options.items():
         OpCodes.append(OpCode(
             (f"op{op}{end}", current_code), None,
+            f"Arithmetic {op} two DATA_{end} numbers and store the result.",
             [
                 PlainParam(type=typ, is_in=True, is_out=False, name="SOURCE1", desc="First operand"),
                 PlainParam(type=typ, is_in=True, is_out=False, name="SOURCE2", desc="Second operand"),
                 PlainParam(type=typ, is_in=False, is_out=True, name="DESTINATION", desc="Destination operand"),
-            ],
-            f"Arithmetic {op} two DATA_{end} numbers and store the result."))
+            ]))
         current_code += 1
 
 for op in ["OR", "AND", "XOR", "RL"]:
@@ -336,22 +338,22 @@ for op in ["OR", "AND", "XOR", "RL"]:
     for typ, end in suffixes.items():
         OpCodes.append(OpCode(
             (f"op{op}{end}", current_code), None,
+            f"Bitwise {op} two DATA_{end} numbers and store the result.",
             [
                 PlainParam(type=typ, is_in=True, is_out=False, name="SOURCE1", desc="First operand"),
                 PlainParam(type=typ, is_in=True, is_out=False, name="SOURCE2", desc="Second operand"),
                 PlainParam(type=typ, is_in=False, is_out=True, name="DESTINATION", desc="Destination operand"),
-            ],
-            f"Bitwise {op} two DATA_{end} numbers and store the result."))
+            ]))
         current_code += 1
     current_code += 1  # LEGO did not use the remaining non-float slot
 
 OpCodes += [
     OpCode(("opINIT_BYTES", 0x2F), None,
+           "Move LENGTH number of DATA8 from BYTE STREAM to memory DESTINATION START",
            [
                T.OUT_8("DESTINATION", "First element in DATA8 array to be initiated"),
                T.IN_8_MANY("SOURCE", "Variadic array with which to initialize the array")
-           ],
-           "Move LENGTH number of DATA8 from BYTE STREAM to memory DESTINATION START")
+           ])
 ]
 
 current_code = 0x30
@@ -363,37 +365,37 @@ for t1, n1 in suffixes.values():
     for t2, n2 in suffixes.values():
         OpCodes.append(OpCode(
             (f"opMOVE{n1}_{n2}", current_code), None,
+            f"Move a DATA_{n1} value to a DATA_{n2} slot.",
             [
                 PlainParam(type=t1, is_in=True, is_out=False, name="SOURCE", desc="Source value"),
                 PlainParam(type=t2, is_in=False, is_out=True, name="DESTINATION", desc="Destination value"),
-            ],
-            f"Move a DATA_{n1} value to a DATA_{n2} slot."))
+            ]))
         current_code += 1
 
 OpCodes += [
     OpCode(("opJR", 0x40), None,
+           "Branch unconditionally relative",
            [
                T.IN_32("OFFSET", "Branch target, relative to ?")
-           ],
-           "Branch unconditionally relative"),
+           ]),
     OpCode(("opJR_FALSE", 0x41), None,
+           "Branch relative if FLAG is FALSE (zero)",
            [
                T.IN_8("FLAG", "Flag upon which to decide the action"),
                T.IN_32("OFFSET", "Branch target, relative to ?")
-           ],
-           "Branch relative if FLAG is FALSE (zero)"),
+           ]),
     OpCode(("opJR_TRUE", 0x42), None,
+           "Branch relative if FLAG is TRUE (non zero)",
            [
                T.IN_8("FLAG", "Flag upon which to decide the action"),
                T.IN_32("OFFSET", "Branch target, relative to ?")
-           ],
-           "Branch relative if FLAG is TRUE (non zero)"),
+           ]),
     OpCode(("opJR_NAN", 0x43), None,
+           "Branch relative if VALUE is NAN (not a number)",
            [
                T.IN_F("VALUE", "Value upon which to decide the action"),
                T.IN_32("OFFSET", "Branch target, relative to ?")
-           ],
-           "Branch relative if VALUE is NAN (not a number)"),
+           ]),
 ]
 
 current_code = 0x44
@@ -407,102 +409,102 @@ for op in ops:
     for typ, end in suffixes:
         OpCodes.append(OpCode(
             (f"opCP_{op}{end}", current_code), None,
+            f"If LEFT is {op} RIGTH - set FLAG",
             [
                 PlainParam(type=typ, is_in=True, is_out=False, name="LEFT", desc="First operand"),
                 PlainParam(type=typ, is_in=True, is_out=False, name="RIGHT", desc="Second operand"),
                 T.OUT_8("FLAG", "1 if condition holds, 0 otherwise")
-            ],
-            f"If LEFT is {op} RIGTH - set FLAG"))
+            ]))
         OpCodes.append(OpCode(
             (f"opJR_{op}{end}", adjacent_code), None,
+            f"Branch relative OFFSET if LEFT is {op} RIGHT",
             [
                 PlainParam(type=typ, is_in=True, is_out=False, name="LEFT", desc="First operand"),
                 PlainParam(type=typ, is_in=True, is_out=False, name="RIGHT", desc="Second operand"),
                 T.IN_32("OFFSET", "Branch target, relative to ?")
-            ],
-            f"Branch relative OFFSET if LEFT is {op} RIGHT"))
+            ]))
         current_code += 1
         adjacent_code += 1
 
 for typ, end in suffixes:
     OpCodes.append(OpCode(
         (f"opSELECT{end}", current_code), None,
+        f"Select a DATA_{end} depending on bool value of a flag",
         [
             T.IN_8("FLAG", "True -> select first, False -> select second"),
             PlainParam(type=typ, is_in=True, is_out=False, name="SOURCE1", desc="First value (True)"),
             PlainParam(type=typ, is_in=True, is_out=False, name="SOURCE2", desc="Second value (False)"),
             PlainParam(type=typ, is_in=False, is_out=True, name="*RESULT", desc="Destination"),
-        ],
-        f"Select a DATA_{end} depending on bool value of a flag"))
+        ]))
     current_code += 1
 
 OpCodes += [
     OpCode(("opSYSTEM", 0x60), None,
+           "Executes a system command",
            [
                T.IN_8("COMMAND", "Command string (HND)"),
                T.OUT_32("STATUS", "Return status of the command")
-           ],
-           "Executes a system command"),
+           ]),
     OpCode(("opPORT_CNV_OUTPUT", 0x61), None,
+           "Convert encoded port to Layer and Bitfield",
            [
                T.IN_32("PortIn", "EncodedPortNumber"),
                T.OUT_8("Layer", "Layer"),
                T.OUT_8("Bitfield", "Bitfield"),
                T.OUT_8("Inverted", "True if left/right motor are inverted (ie, C&A)"),
-           ],
-           "Convert encoded port to Layer and Bitfield"),
+           ]),
     OpCode(("opPORT_CNV_INPUT", 0x62), None,
+           "Convert encoded port to Layer and Port",
            [
                T.IN_32("PortIn", "EncodedPortNumber"),
                T.OUT_8("Layer", "Layer"),
                T.OUT_8("PortOut", "0-index port for use with VM commands")
-           ],
-           "Convert encoded port to Layer and Port"),
+           ]),
     OpCode(("opNOTE_TO_FREQ", 0x63), None,
+           "Convert note to tone",
            [
                T.IN_8("NOTE", "Note string (e.c. 'C#4')"),
                T.OUT_16("FREQ", "Frequency [Hz]")
-           ],
-           "Convert note to tone"),
+           ]),
 ]
 
 OpCodes += [
     OpCode(("opINFO", 0x7C), ("SET_ERROR", 1),
+           "Push new error to VM error queue.",
            [
                T.IN_8("NUMBER", "Error number")
-           ],
-           "Push new error to VM error queue."),
+           ]),
     OpCode(("opINFO", 0x7C), ("GET_ERROR", 2),
+           "Pop an error from VM error queue.",
            [
                T.OUT_8("NUMBER", "Error number")
-           ],
-           "Pop an error from VM error queue."),
+           ]),
     OpCode(("opINFO", 0x7C), ("ERRORTEXT", 3),
+           "Convert error number to text string",
            [
                T.IN_8("NUMBER", "Error number"),
                T.IN_8("LENGTH", "Maximal length of string returned (-1 = no check)"),
                T.OUT_8("DESTINATION", "String variable or handle to string")
-           ],
-           "Convert error number to text string"),
+           ]),
 
     OpCode(("opINFO", 0x7C), ("GET_VOLUME", 4),
+           "Get brick volume",
            [
                T.OUT_8("VALUE", "Volume [0..100%]")
-           ],
-           "Get brick volume"),
+           ]),
     OpCode(("opINFO", 0x7C), ("SET_VOLUME", 5),
+           "Set brick volume",
            [
                T.IN_8("VALUE", "Volume [0..100%]")
-           ],
-           "Set brick volume"),
+           ]),
     OpCode(("opINFO", 0x7C), ("GET_MINUTES", 6),
+           "Get inactive time before entering sleep",
            [
                T.OUT_8("VALUE", "Minutes to sleep [0..120min] (0 = ~)")
-           ],
-           "Get inactive time before entering sleep"),
+           ]),
     OpCode(("opINFO", 0x7C), ("SET_MINUTES", 7),
+           "Set inactive time before entering sleep",
            [
                T.IN_8("VALUE", "Minutes to sleep [0..120min] (0 = ~)")
-           ],
-           "Set inactive time before entering sleep"),
+           ]),
 ]
